b5e4fe9d8a41be422053042bcec6d55c6d4765ee
daveyqyou <yongqiang.you@gmail.com>
2018-09-12 16:36:55 +0800

    Update weixinInfo.h

--- YQWeChatLoginDemo/weixinInfo.h
+++ YQWeChatLoginDemo/weixinInfo.h
@@ -10,8 +10,8 @@
 #define weixinInfo_h
 
 
-#define WXPatient_App_ID                 @"wxfa555034029067ae" //您的帐号id信息
-#define WXPatient_App_Secret             @"ee41396c30641f412483bd3638fc9601" //您的帐号secret信息
+#define WXPatient_App_ID                 @"" //您的帐号id信息
+#define WXPatient_App_Secret             @"" //您的帐号secret信息
 #define WX_BASE_URL                      @"https://api.weixin.qq.com/sns"
 #define WX_ACCESS_TOKEN                  @"access_token"
 #define WX_OPEN_ID                       @"openid"
